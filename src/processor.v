20509cacc3b4f61c913c0ca7c56295258d0c4a9a
Kriish Tiwari <kriishti@buffalo.edu>
2026-03-31 21:18:50 -0400

    processors

--- src/processor.v
+++ src/processor.v
@@ -18,11 +18,56 @@ module processor_top( // declaration of processor module which will instantiate 
     wire [15:0] mem_read_data;
     
 
-    wire [15:0] reg1 = 16'b0; //holds the value from source register
-    wire [15:0] reg2 = 16'b0; //holds the value frm destination register
+    wire [15:0] reg1; //holds the value from source register
+    wire [15:0] reg2; //holds the value frm destination register
+    wire [15:0] wbdata;
+    wire RegWrite, ALUSrc, MemRead, MemWrite, MemToReg, Branch, BranchNE, Jump;
+    wire [3:0] ALUCtrl;
+    wire zero;
+    wire branchDecision;
 
-   
-    assign pc_next = pc_current + 16'd2;
+    wire [15:0] pcplus2;
+    wire [15:0] branchTarget;
+    wire [15:0] jumpTarget;
+    wire [15:0] PCBranchTarget;
+    wire [15:0] jumpsignExt;
+    wire [11:0] jumpAddr = instruction[11:0];
+    wire [3:0]  funct = instruction[3:0];
+
+    assign pcplus2       = pc_current + 2;
+    assign branchTarget  = pcplus2 + (imm_ext << 1); // here we are calculating branchTaken
+                                                      //address by adding sign extnd imm to pc+2
+    assign jumpTarget    = pcplus2 + (jumpsignExt << 1); //same but for jump
+    assign branchDecision = Branch & ((~BranchNE & zero) | (BranchNE & ~zero)); // we are basically calculating
+                                                                                // whether branch will be taken or not based on the zero flag and branch signals
+
+
+
+    control_unit cntrlunt ( //takes opcode (+funct if needed ) and generates control signal
+    .opcode(opcode),
+    .funct(funct),
+    .RegWrite(RegWrite),
+    .ALUSrc(ALUSrc),
+    .MemRead(MemRead),
+    .MemWrite(MemWrite),
+    .MemToReg(MemToReg),
+    .Branch(Branch),
+    .BranchNE(BranchNE),
+    .Jump(Jump),
+    .ALUCtrl(ALUCtrl)
+    );
+
+    register_file regfile (
+    .clock(clk),
+    .registerWrite(RegWrite),
+    .rs(rs),
+    .rtrd(rtrd),
+    .writeData(wbdata),
+    .readData1(reg1),
+    .readData2(reg2)
+    );
+
+    
 
     program_counter pc ( //module declaration for program counter
         .clock(clk),
@@ -41,27 +86,53 @@ module processor_top( // declaration of processor module which will instantiate 
         .imm_out(imm_ext)
     );
 
+    sign_extend_12 se_jump ( // used for jump addr calculation
+    .addr_in(jumpAddr),
+    .addr_out(jumpsignExt)
+    );
+
     mux2to1 alu_mux ( //module declaration for mux to select between reg value and immediate value for second input to ALU
-        .in0(reg2),
+        .in0(reg1),
         .in1(imm_ext),
-        .sel(1'b1),
+        .sel(ALUSrc),
         .out(alu_in_b)
     );
 
     alu alu_unit ( // module declaration for ALU, takes the opcode as control signal to determine the operation to perform on the inputs
-        .A(reg1),
+        .A(reg2),
         .B(alu_in_b),
-        .ALUCtrl(opcode),
+        .ALUCtrl(ALUCtrl),
         .Result(alu_result),
-        .Zero()
+        .Zero(zero)
     );
 
     data_memory dmem (
         .clk(clk),
-        .MemRead(1'b0),
-        .MemWrite(1'b0),
+        .MemRead(MemRead),
+        .MemWrite(MemWrite),
         .addr(alu_result),
-        .write_data(reg2),
+        .write_data(reg1),
         .read_data(mem_read_data)
+    );
+
+    mux2to1 wbmux (
+    .in0(alu_result),
+    .in1(mem_read_data),
+    .sel(MemToReg),
+    .out(wbdata)
+    );
+
+    mux2to1 branchmux (
+    .in0(pcplus2),
+    .in1(branchTarget),
+    .sel(branchDecision),
+    .out(PCBranchTarget)
+);
+
+    mux2to1 jumpmux (
+    .in0(PCBranchTarget),
+    .in1(jumpTarget),
+    .sel(Jump),
+    .out(pc_next)
     );
 endmodule
---
+++ src/program_counter.v
@@ -0,0 +1,13 @@
+module program_counter(
+    input clock,
+    input resetProc,
+    input  [15:0] pcNext,  
+    output reg [15:0] pc     
+);
+    always @(posedge clock or posedge resetProc) begin
+        if (resetProc == 1)
+            pc <= 0;
+        else
+            pc <= pcNext;
+    end
+endmodule
