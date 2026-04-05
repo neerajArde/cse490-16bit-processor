fff4940b1aab04e01b514733dd9b247fc2f2693d
Kriish Tiwari <kriishti@buffalo.edu>
2026-04-05 18:42:55 -0400

    formatting changes

--- testbench/processor_tb.v
+++ testbench/processor_tb.v
@@ -33,66 +33,64 @@ module processor_tb;
        
        // THIS IS THE VERIFICATION PART OF TGE TEST BENCH
         #5 // tesing for addi $s1, $s0, 5 
-        $display("checking addi : RF_WD=%04h (expect 0005) %s",
+        $display("checking addi : RF_WD=%04h (expected is 0005) %s",
             proc_inst.wbdata,
             (proc_inst.wbdata == 16'h0005) ? "passed addi $s1, $s0, 5" : "failed addi $s1, $s0, 5");
         #10;        // addi $s2, $s0, 3 progCounter=0002
-        $display("checking 2nd addi : RF_WD=%04h (expect 0003) %s",
+        $display("checking 2nd addi : RF_WD=%04h (expected is 0003) %s",
             proc_inst.wbdata,
             (proc_inst.wbdata == 16'h0003) ? "passed addi $s2, $s0, 3" : "failed addi $s2, $s0, 3");
 
         
         #10; // add  progCounter=0004
-        $display("checking  add  : RF_WD=%04h (expect 0008) %s",
+        $display("checking  add  : RF_WD=%04h (expected is 0008) %s",
             proc_inst.wbdata,
             (proc_inst.wbdata == 16'h0008) ? "passed add" : "failed add");
 
         // sub progCounter=0006
         #10;
-        $display("checking  sub  : ALU=%04h   (expect fffb) %s",
+        $display("checking  sub  : ALU=%04h   (expected is fffb) %s",
             proc_inst.alu_result,
             (proc_inst.alu_result == 16'hfffb) ? "passed sub" : "failed sub");
 
 
         // and  progCounter=0008
         #10;
-        $display("checking  and  : RF_WD=%04h (expect 0003) %s",
+        $display("checking  and  : RF_WD=%04h (expected is 0003) %s",
             proc_inst.wbdata,
             (proc_inst.wbdata == 16'h0003) ? "passed and" : "failed and");
         
         // sll  progCounter=000a
         #10;
-        $display("checking sll  : RF_WD=%04h (expect 0018) %s",
+        $display("checking sll  : RF_WD=%04h (expected is 0018) %s",
             proc_inst.wbdata,
             (proc_inst.wbdata == 16'h0018) ? "passed sll" : "failed sll");
  
-        // sw 
+        // load and store word 
         #10;
-        $display("checking sw   : MemWr=%b   (expect 1)    %s",
+        $display("checking sw   : MemWr=%b (expected is 1) %s",
             proc_inst.MemWrite,
             (proc_inst.MemWrite == 1'b1) ? "passed sw" : "failed sw");
- 
-        // lw  
-        #10;
-        $display("checking lw   : DMEM_RD=%04h (expect 0018) %s",
+         #10;
+        $display("checking lw   : DMEM_RD=%04h (expected is 0018) %s",
             proc_inst.mem_read_data,
             (proc_inst.mem_read_data == 16'h0018) ? "passed lw" : "failed lw");
          
         #10; // now testing beq
-        $display("checking beq  : Branch=%b Zero=%b BranchDec=%b (expect Branch=1 Zero=0 BranchDec=0) %s",
+        $display("checking beq  : Branch=%b Zero=%b BranchDec=%b (expected is Branch=1 Zero=0 BranchDec=0) %s",
             proc_inst.Branch,
             proc_inst.zero,
             proc_inst.branchDecision,
         (proc_inst.Branch == 1'b1 && proc_inst.zero == 1'b1 && proc_inst.branchDecision == 1'b1) ? "passed beq" : "failed beq");        
         #10;
-        $display("checking addi skipped : RF_WD=%04h (expect 0000 - skipped by beq) %s",
+        $display("checking addi skipped : RF_WD=%04h (expected is 0000 - skipped by beq) %s",
             proc_inst.wbdata,
             (proc_inst.wbdata == 16'h0000) ? "passed addi skipped" : "failed addi skipped");
-        $display("checking jump : Jump=%b    (expect 1)    %s",
+        $display("checking jump : Jump=%b    (expected is 1)    %s",
             proc_inst.Jump,
             (proc_inst.Jump == 1'b1) ? "passed jump" : "failed jump");
         #10; //final check to ensure prgcount back to 0 affter jump
-        $display("checking jump landing: PC=%04h (expect 0000) %s",
+        $display("checking jump landing: PC=%04h (expected is 0000) %s",
             proc_inst.pc_current,
             (proc_inst.pc_current == 16'h0000) ? "passed jump landing" : "failed jump landing");
 
